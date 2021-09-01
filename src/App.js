b452c6cf3a0b6964ea334878e48a80ef47226f22
Adam <chillcoders@gmail.com>
2021-09-01 19:32:11 -0400

    rect

--- src/App.js
+++ src/App.js
@@ -10,36 +10,26 @@ function App() {
     let x = (p5.windowWidth - p5.width) / 2;
     let y = (p5.windowHeight - p5.height) / 2;
     xyz.position(x, y);
-    p5.textAlign(p5.CENTER);
-  p5.textSize(18);
-  p5.fill(255);
-  p5.sentenceArray = p5.sentence.p5.split(""); // splits a string into an array of chars
-
-    p5.print(p5.sentenceArray);
   };
   let draw = (p5) => {
-    p5.background("rgb(100%,0%,10%)");
+    p5.background("rgb(100%,0%,5%)");
     p5.stroke(255);
-    p5.strokeWeight(4);
+    p5.strokeWeight(6);
     p5.noFill();
-    p5.ellipse(a, b, 100, 100);
-
+    p5.rect(a, b, 100, 100);
     if (a >= p5.width) {
+      speed = -4;
+    }
+    if (a === 90) {
+      speed = 3;
+    }
+    if (a >= p5.height) {
       speed = -3;
     }
     if (a === 90) {
       speed = 3;
     }
     a = a + speed;
-
-    p5.translate(p5.width / 3, p5.height / 3);
-  let x = p5.r * p5.cos(p5.theta);
-  let y = p5.r * p5.sin(p5.theta);
-
-  for (let i = 0; i < p5.sentenceArray.length; i++) {
-    p5.rotate(p5.QUARTER_PI / 1.25);
-    p5.text(p5.sentenceArray[i], x, y);
-  }
   };
   return (
     <div className="App">
